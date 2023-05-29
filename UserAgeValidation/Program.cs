b6a65bc320995e7f4a23d7394f7bf2cde57a0574
Rix <rihard.konnapere@tktk.ee>
2023-05-29 16:30:41 +0300

    add week 2 code

---
+++ UserAgeValidation/Program.cs
@@ -0,0 +1,22 @@
+﻿//rakendus määrab kasutaja soo ning nime ja tervitab korrektselt
+
+Console.WriteLine("Please select your gender (m/f):");
+char userGender = Char.Parse(Console.ReadLine());
+
+Console.WriteLine("Please write your last name:");
+string userName = Console.ReadLine();
+
+if (userGender == 'm')
+{
+    Console.WriteLine($"Welcome, Mr.{userName}!");
+}
+else if (userGender == 'f')
+{
+    Console.WriteLine($"Welcome, Ms.{userName}!");
+}
+else
+{
+    Console.WriteLine($"Welcome {userName}!");
+}
+
+
---
+++ UserAgeValidation1/Program.cs
@@ -0,0 +1,26 @@
+﻿
+Console.WriteLine("Enter your age:");
+
+
+string userAge = Console.ReadLine();
+
+int userAgeNum = 0;
+
+bool isAgeNumber = Int32.TryParse(userAge, out userAgeNum);
+
+
+if (isAgeNumber == true)
+{
+    if (userAgeNum >= 13)
+    {
+        Console.WriteLine("Welcome to Instagram!");
+    }
+    else
+    {
+        Console.WriteLine("You are too young for Instagram.");
+    }
+}
+else 
+{
+    Console.WriteLine("Could not read your age.");
+}
